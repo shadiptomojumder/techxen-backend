e50da294f7675c34db7c7b823f99101e7e9b15ee
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-04-25 13:10:52 +0600

    Fix: Export app instance for Vercel deployment

---
+++ vercel/index.js
@@ -0,0 +1,8 @@
+import app from "../src/app.js";
+import connectDB from "../src/db/dbConnection.js";
+import serverless from "serverless-http";
+
+// Connect to DB when function is first loaded
+await connectDB();
+
+export const handler = serverless(app);
