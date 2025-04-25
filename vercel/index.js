f619065e98d24a2528bd0fffdf9cead4b5504e5f
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-04-25 13:27:13 +0600

    small change

--- vercel/index.js
+++ vercel/index.js
@@ -2,7 +2,7 @@ import app from "../src/app.js";
 import connectDB from "../src/db/dbConnection.js";
 import serverless from "serverless-http";
 
-// Connect to DB when function is first loaded
+// Connect to DB when function is first loaded 
 await connectDB();
 
 export const handler = serverless(app);
